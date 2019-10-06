895eb40998e3f99552a7885a565cc09388a34aec
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-06 06:20:32 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,17 @@
-def square_array(array)
-  # your code here
+def square_array [1,2,3]
+numbers = [1,2,3]
+i = 1
+while i <= numbers.lenght
+puts i + 1 ** i
 end
+
+def square_array [9,10,16,25]
+square_array == [9,10,16,25]
+i = square_array[1]
+while i <= square_array.lenght
+puts i ** i.each(i)
+end
+
+ 
+
+
