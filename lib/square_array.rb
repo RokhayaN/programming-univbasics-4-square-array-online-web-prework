890c2fc2890c16f7d8160e5ab09ba4aea48d0082
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-06 17:40:29 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -6,7 +6,7 @@ puts i + 1 ** i
 end
 
 def square_array [9,10,16,25]
-square_array == [9,10,16,25]
+square_array = [9,10,16,25]
 i = square_array[1]
 while i <= square_array.lenght
 puts i ** i.each(i)
